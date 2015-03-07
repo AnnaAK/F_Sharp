dc9dc5b9f01fb7f43684c9d045d3270353f5d1c1
AnnaAK <anna.kudryashova.va@gmail.com>
2015-03-07 10:26:46 +0300

    Create Tree02.fs
    Exercise 14 - 19 from homework

---
+++ Tree02.fs
@@ -0,0 +1,42 @@
+module Tree02
+
+(* Exercise 14 - 19 from homework dated March 2
+Author : Kudryashova Anna
+Expected time: 4 hours
+Real time:   hours *)
+
+type Trees<'A> = Empty | Tree of 'A * Trees<'A> * Trees<'A>
+ 
+
+let  rec insert e t =
+  match e, t with
+  | e, Empty -> Tree(e, Empty, Empty)
+  | e, Tree(p, lc, rc) -> 
+  if e > p then Tree(p, lc, insert e rc)
+  else if e < p then Tree (p, insert e lc, rc)
+  else t
+
+
+let rec map f t =
+  match t with
+  | Empty -> Empty
+  | Tree (p, lc, rc) -> Tree(f p, map f lc, map f rc)
+
+let mapTree t = map (fun i -> i + 2.0) t
+
+
+
+[<EntryPoint>]
+let main args =
+  let  tr = Tree(9.0, Empty, Empty)
+  printf "\n"
+  let tr = insert 7.1 tr
+  let tr = insert 8.4 tr
+  let tr = insert 10.3 tr
+  let tr = insert 5.7 tr
+  let tr = insert 3.1 tr
+  printf "original tree %A\n" tr
+  printf "maping tree (i + 2): %A\n" (mapTree tr)
+  printf "\n"
+  
+  0
