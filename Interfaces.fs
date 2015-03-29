271fca38244f9972e65c2836e0e24e8e720e9232
AnnaAK <anna.kudryashova.va@gmail.com>
2015-03-29 03:36:45 +0300

    fixed 25
    интерфейс для полиморфного списка перенесен в другой файл

--- Interfaces.fs
+++ Interfaces.fs
@@ -2,30 +2,12 @@
  Expected time: 3 hours
  Real time: 2 hours *)
  
- // 20. Интерфейс ориентированного графа
- type IGraph<'A> =
-  interface
-    abstract Number : unit -> int
-    abstract Edge : int -> int -> bool
-    abstract Val  : int -> 'A
-  end
  
 // 25. Интерфейс полиморфного помеченного графа 
-type IGraph2<'A> = 
+type IMarkGraph<'A> = 
   interface
-    inherit IGraph<'A>
-    abstract PolyEdge : 'A -> 'A -> bool
+    inherit IGraph
+    abstract Val  : int -> 'A
   end
   
-  // 27. Интерфейс для полиморфного списка
-  type IList<'A> = 
-  interface
-    abstract AddBegin  : 'A -> IList<'A>
-    abstract AddEnd    : 'A -> IList<'A>
-    abstract AddNumber : 'A -> int -> IList<'A>
-    abstract RemBegin   : unit -> IList<'A>
-    abstract RemEnd    :  unit -> IList<'A>
-    abstract RemNumber : int -> IList<'A>
-    abstract Search    : ('A -> bool) -> Option<'A>
-    abstract Concat    : IList<'A> -> IList<'A>
-  end
+
