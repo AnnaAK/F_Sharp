ff1b4e46e7bd7e80f6f3a083761c5c593a9cffa1
AnnaAK <anna.kudryashova.va@gmail.com>
2015-04-21 10:50:20 +0400

    add hw07

--- hw07/src/app/ex40_1.fs
+++
@@ -1,9 +0,0 @@
-module ex40_1
-
-open System
-
-type RingBuilder (vals: int) =
-  member this.Bind (x, f) = f (x % vals)
-    
-  member this.Return x = if (x % vals >= 0) then (x % vals) else ( vals + x % vals)
-
