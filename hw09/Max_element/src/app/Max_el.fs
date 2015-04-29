0739c36c17c5f977546de3073f29c611426d7b5b
AnnaAK <anna.kudryashova.va@gmail.com>
2015-04-29 19:46:32 +0400

    add hw09 ex.46

---
+++ hw09/Max_element/src/app/Max_el.fs
@@ -0,0 +1,39 @@
+﻿module Max_el
+open System.Threading
+open System.Diagnostics
+
+let max_el (ls : int list) l r : int =
+  let mutable res = ls.[0]
+  for i in l .. r do
+    if ls.[i] > res then res <- ls.[i]
+    printf "%A\n" ls.[i]
+  res
+
+let max_t  (list : int list) threadNumber  =
+  let lst = list
+  let size = lst.Length 
+  let res = ref -2147483648
+  let step = size / threadNumber
+  let threadList = List.init threadNumber (fun i ->
+      new Thread(ThreadStart(fun _ ->
+          let threadRes = max_el lst (i * step) ((i+1) * step - 1)
+          if threadRes > res.Value then res:= threadRes
+        ))
+    )
+  for t in threadList do
+    t.Start()
+  for t in threadList do
+    t.Join()
+  
+  res.Value
+
+let diagnostic t l =
+  let timer = new Stopwatch()
+  timer.Start()
+  let returnValue = max_t l t
+  printfn "Count: %d" t
+  printfn "Time: %i" timer.ElapsedMilliseconds
+  returnValue 
+
+
+
---
+++ hw09/Max_element/src/test/test_MaxEl.fs
@@ -0,0 +1,44 @@
+﻿module test_MaxEl
+open Max_el
+open NUnit.Framework
+
+[<Test>] // положительные один поток
+let ``positive: 1 thread ``() =
+    let list = [1..100]
+    let res = max_t list 1
+    Assert.AreEqual (res, 100) 
+
+[<Test>] // положительные четыре потока
+let ``positive: 4 thread ``() =
+    let list = [1..100]
+    let res = max_t list 4
+    Assert.AreEqual (res, 100) 
+
+[<Test>] // отрицательные два потока
+let ``negative: 2 thread ``() =
+    let list = [-1000..-1]
+    let res = max_t list 2
+    Assert.AreEqual (res, -1) 
+
+[<Test>] // отрицательные один поток
+let ``negative: 1 thread ``() =
+    let list = [-1000..-1]
+    let res = max_t list 1
+    Assert.AreEqual (res, -1) 
+
+
+[<Test>] // смешанное два потока
+let ``2 thread ``() =
+    let list = [-1; 3; 0; 10; -100; -2222; 33; -5]
+    let res = max_t list 2
+    Assert.AreEqual (res, 33) 
+
+[<Test>] // смешанное один поток
+let ``1 thread ``() =
+    let list = [-1; 3; 0; 10; -100; -2222; 33; -5]
+    let res = max_t list 1
+    Assert.AreEqual (res, 33) 
+
+
+
+
